feba830a824d73ac360dd8bf0ad0b6b3607faf02
Albert Mink <albert.mink@sap.com>
2025-06-25 16:29:04 +0200

    wip

---
+++ extension.js
@@ -0,0 +1,21 @@
+const vscode = require('vscode');
+
+function activate(context) {
+  const config = vscode.workspace.getConfiguration('workbench.editor.customLabels');
+  const patterns = {
+    "**/*.clas.abap": "${dirname} - Global Class",
+    "**/*.clas.implementations.abap": "${dirname} - Locale Types",
+    "**/*.clas.definitions.abap": "${dirname} - Class-relevant Locale Types",
+    "**/*.clas.testclasses.abap": "${dirname} - Test Classes",
+    "**/*.clas.macros.abap": "${dirname} - Macros"
+  };
+
+  config.update('patterns', patterns, vscode.ConfigurationTarget.Global);
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate
+};
